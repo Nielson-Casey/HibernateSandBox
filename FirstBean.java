be7ea54265b4cb6b373cef1a87f040657159c896
Nielson-Casey <chif346@gmail.com>
2014-10-21 19:30:50 -0600

    Update Java Bean

--- FirstBean.java
+++ FirstBean.java
@@ -4,7 +4,32 @@ public class FirstBean implements java.io.Serializable {
 
 	/**This defines which version**/
 	private static final long serialVersionUID = -879972165483850L;
-
+	private String userName;
+	private String password;
+	int length;
+	int width;
+	public FirstBean(){}
+	public FirstBean(int x, int y)
+	{
+		
+		this.length = 0;
+		this.width = 0;
+	}
 	
-	
+	public String getUserName()
+	{
+		return userName;
+	}
+	public String getPassword()
+	{
+		return password;
+	}
+	public String setUserName()
+	{
+		return userName;
+	}
+	public String setPassword()
+	{
+		return password;
+	}
 }
